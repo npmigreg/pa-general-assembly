9314dea3135bbe8c91687aca79c037d1a5bc2746
AI-gregking <gregory.king@arbitratorintelligence.com>
2021-03-12 18:02:02 -0500

    Updated settings

--- pa_analysis/settings.py
+++ pa_analysis/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -120,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
