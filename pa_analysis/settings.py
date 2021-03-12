4cd9e8e1c2abf41808fbc2f1831de3f6f4df7291
AI-gregking <gregory.king@arbitratorintelligence.com>
2021-03-11 23:47:16 -0500

    Added environment variables setup

--- pa_analysis/settings.py
+++ pa_analysis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'esx#qm8(mm!+w(m0z(bc^_c8$6jsmcrg(8y3cv19+ae811k5$n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
