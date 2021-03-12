310dbc690f7c0b6e739ab39fbed4a207381be6e5
AI-gregking <gregory.king@arbitratorintelligence.com>
2021-03-12 18:29:25 -0500

    Added django-heroku

--- pa_analysis/settings.py
+++ pa_analysis/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
