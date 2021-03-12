1027f64f077188f38782ff194717c20c68de3c01
AI-gregking <gregory.king@arbitratorintelligence.com>
2021-03-12 18:23:32 -0500

    Updated settings

--- pa_analysis/settings.py
+++ pa_analysis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['pa-generalassembly-gk.herokuapp.com/']
+ALLOWED_HOSTS = ['pa-generalassembly-gk.herokuapp.com']
 
 
 # Application definition
