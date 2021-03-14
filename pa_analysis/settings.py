e4e5cbdfa1ff44325440af8a7a9b432b48d8239a
AI-gregking <gregory.king@arbitratorintelligence.com>
2021-03-13 20:26:41 -0500

    Updated settings

--- pa_analysis/settings.py
+++ pa_analysis/settings.py
@@ -135,4 +135,4 @@ CELERY_BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux 
 CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
 CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
-CELERY_WORKER_CONCURRENCY = 10 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
+CELERY_WORKER_CONCURRENCY = 1 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
